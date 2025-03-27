567812ba17987713e3c793c334b7910743995623
Jose Uriel Contreras Arevalos <128828694+Urielcont@users.noreply.github.com>
2025-03-27 00:36:54 -0600

    Creacion de rutas para usuarios y documentacion con swagger

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -76,7 +76,160 @@ router.post(
     }),
   );
 
-module.exports = router;
+
+  
+/**
+ * @swagger
+ * /user/registrar:
+ *   post:
+ *     summary: Registrar un nuevo usuario
+ *     description: Registrar datos de un nuevo usuario
+ *     tags:
+ *       - Usuarios
+ *     requestBody:
+ *       required: true
+ *       content:
+*         application/json:
+*           schema:
+*             type: object
+*             properties:
+*               nombre:
+*                 type: string
+*                 description: Nombre del trabajador.
+*                 example: "Juan"
+*               apellido_paterno:
+*                 type: string
+*                 description: Apellido Paterno
+*                 example: 'Contreras'
+*               apellido_materno:
+*                 type: string
+*                 description: Apellido Materno
+*                 example: 'Soto'
+*               email:
+*                 type: string
+*                 description: Correo electronico para iniciar sesion
+*                 example: 'asd@gmail.com'
+*               password:
+*                 type: string
+*                 description: Contraseña para iniciar sesion
+*                 example: '123232'
+*               departamento:
+*                 type: string
+*                 description: Departamento en el que trabaja la persona
+*                 example: 'Recursos Humanos'
+*               puesto:
+*                 type: string
+*                 description: Puesto que tiene la persona
+*                 example: 'Supervisor'
+ *     responses:
+ *       201:
+ *         description: Registro exitoso de usuario
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Registro Exitoso"
+ *       400:
+ *         description: Error de Servidor
+
+ *       404:
+ *         description: Error de Servidor
+ *       405:
+ *         description: Metodo no permitido
+ *       500:
+ *         description: Error interno del servidor
+ *       503:
+ *         description: Servidor no disponible
+ */
+  router.post(
+    "/registrar",
+    celebrate({
+      [Segments.BODY]: Joi.object({
+        nombre: Joi.string().min(3).max(250).required().messages(userValidation.nombre),
+        apellido_paterno: Joi.string().min(3).max(250).required().messages(userValidation.apellido_paterno),
+        apellido_materno: Joi.string().min(3).max(250).required().messages(userValidation.apellido_materno),
+        email: Joi.string().min(3).max(250).email().messages(userValidation.email),
+        password: Joi.string().required().messages(userValidation.password),
+        departamento: Joi.string().min(3).max(250).required().messages(userValidation.departamento),
+        puesto: Joi.string().min(3).max(250).required().messages(userValidation.puesto),
+      }),
+    }),
+  );
+
+
+/**
+ * @swagger
+  * /user/logout:
+  *   post:
+  *     summary: Cerrar Sesion
+  *     description: Cerrar sesion del usuario
+  *     tags:
+  *       - Usuarios
+  *     requestBody:
+  *       required: false
+  *     responses:
+  *       201:
+  *         description: Cierre de sesion exitoso
+  *         content:
+  *           application/json:
+  *             schema:
+  *               type: object
+  *               properties:
+  *                 message:
+  *                   type: string
+  *                   example: "Cierre de sesion exitoso"
+  *       400:
+  *         description: Error de Servidor
+  *       401:
+  *         description: Error de autenticacion
+  *       404:
+  *         description: Error de Servidor
+  *       405:
+  *         description: Metodo no permitido
+  *       500:
+  *         description: Error interno del servidor
+  *       503:
+  *         description: Servidor no disponible
+  */
+router.post("/logout",);
+
+/**
+ * @swagger
+ * /user/usuarios:
+ *   get:
+ *     summary: Obtener datos de todos los usuarios
+ *     description: Devuelve un json con los datos de todos los usuarios
+ *     tags:
+ *       - Usuarios
+ *     responses:
+ *       200:
+ *         description: Datos de los usuario obtenidos correctamente
+ *       400:
+ *        description: Error de Servidor
+ */
+router.get("/usuarios", );
+
+
+/**
+ * @swagger
+ * /user/perfil:
+ *   get:
+ *     summary: Obtener datos de un usuario en especifico
+ *     description: Devuelve un json con los datos del usuario en especifico.
+ *     tags:
+ *       - Usuarios
+ *     responses:
+ *       200:
+ *         description: Datos del usuario obtenidos correctamente
+ *       400:
+ *        description: Error de Servidor
+ */
+
+router.get("/perfil/:id", );
+ 
 
 export default router;
 
--- src/validator/userValidator.ts
+++ src/validator/userValidator.ts
@@ -3,6 +3,21 @@ export const userValidation = {
         'number.base': '" ID" debe ser un numero',
         'number.empty': '"ID" no puede estar vacío',
     },
+    nombre:{
+        'string.base': '"Nombre" debe ser una cadena de Texto',
+        'string.min': '"Nombre" debe tener al menos 3 caracteres',
+        'string.max': '"Nombre" no puede exceder los 250 caracteres',
+    },
+    apellido_paterno:{
+        'string.base': '"Apellido Paterno" debe ser una cadena de Texto',
+        'string.min': '"Apellido Paterno" debe tener al menos 3 caracteres',
+        'string.max': '"Apellido Paterno" no puede exceder los 250 caracteres',
+    },
+    apellido_materno:{
+        'string.base': '"Apellido Materno" debe ser una cadena de Texto',
+        'string.min': '"Apellido Materno" debe tener al menos 3 caracteres',
+        'string.max': '"Apellido Materno" no puede exceder los 250 caracteres',
+    },
     email: {
         'string.base': '"email" debe ser una cadena de Texto',
         'string.max': '"email" no puede exceder los 250 caracteres',
@@ -11,5 +26,15 @@ export const userValidation = {
     password: {
         'string.base': '"Password" debe ser mayor a 6 caracteres',
         'string.empty': '"Password" no puede estar vacío',
+    },
+    departamento: {
+        'string.base': '"Departamento" debe ser una cadena de Texto',
+        'string.min': '"Departamento" debe tener al menos 3 caracteres',
+        'string.max': '"Departamento" no puede exceder los 250 caracteres',
+    },
+    puesto: {
+        'string.base': '"Puesto" debe ser una cadena de Texto',
+        'string.min': '"Puesto" debe tener al menos 3 caracteres',
+        'string.max': '"Puesto" no puede exceder los 250 caracteres',
     },
   };
